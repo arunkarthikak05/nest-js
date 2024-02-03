6dd498506d816b44aa62e8aee804a8535e0b020f
karthikak05 <akkarthikk2002@gmail.com>
2024-02-03 23:45:26 +0530

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodosModule } from './todos/todos.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TodosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
